b1d2c1767ac80596c0a667dc8df97fc2e90e4342
Codewithpabitra <pabitra05.maity@gmail.com>
2025-11-20 01:31:17 +0530

    day00 commit

---
+++ Day00/warmup.md
@@ -0,0 +1,15 @@
+# Welcome To the world of PYTHON 
+
+`PYTHON` - is a `high level programming language` . It is very easy to learn and feels like english . It supports `OOP` (Object Oriented Programming) also. Don't worry I will be explaining all the terms later on.
+
+### lets write some code - 
+
+```python
+
+print("Hello world") # this print "Hello world" on the screen 
+print("hello from CodeWithPabitra") # this print "hello from CodeWithPabitra"
+
+```
+
+`'print( )'` is a built in function which print (exactly whatever it take) on the screen.
+
---
+++ Day00/warmup.py
@@ -0,0 +1,12 @@
+# This is a comment in python -> we use '#' to write comment and these are not executed by python
+
+# PYTHON - is a high level programming language . It is very easy to learn and feels like english . It supports OOP(Object Oriented Programming) also. Don't worry I will be explaining all the terms later on.
+
+# lets write some code - 
+
+print("Hello world") # this print "Hello world" on the screen 
+print("hello from CodeWithPabitra") # this print "hello from CodeWithPabitra"
+
+
+# ** So, the 'print()' is a built in function which print (exactly whatever it take) on the screen.
+
